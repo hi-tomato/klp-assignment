c64491c80d6e2375b51da80e8f227ce5ec785d0e
이승준 <tmdwns092727@nate.com>
2025-10-16 14:59:26 +0900

    style: 불필요한 StyleSheet 제거

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,7 +1,5 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 export default function ExploreScreen() {
   return <View>Explore Screen</View>;
 }
-
-const styles = StyleSheet.create({});
