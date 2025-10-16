cb6947f334d701185f07ee5204eb5a9065f58491
이승준 <tmdwns092727@nate.com>
2025-10-16 16:09:28 +0900

    feat: Input Field 컴포넌트 구현
    - colors.ts 그레이 색상 추가

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,4 +1,10 @@
 export const colors = {
   BLACK: "#000000",
   WHITE: "#FFFFFF",
+  GRAY_100: "#F6F6F6",
+  GRAY_200: "#E2E8F0",
+  GRAY_300: "#D1D5DB",
+  GRAY_500: "#6B7280",
+  GRAY_600: "#4B5563",
+  GRAY_700: "#374151",
 };
