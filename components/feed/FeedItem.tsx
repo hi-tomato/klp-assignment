ee20f3113d1ce6016731679b032758497a7198d0
이승준 <tmdwns092727@nate.com>
2025-10-17 13:08:57 +0900

    feat: FeedItem 컴포넌트 추가
    - 임시 데이터를 이용하여, 컴포넌트 구성

---
+++ components/feed/FeedItem.tsx
@@ -0,0 +1,74 @@
+import { colors } from "@/constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import { User } from "firebase/auth";
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Profile from "./Profile";
+
+export default function FeedItem({ user }: { user: User }) {
+  const handleMoreOption = () => {
+    // TODO: 삭제, 취소 버튼
+  };
+
+  return (
+    <View style={styles.contentContainer}>
+      <Profile
+        user={user}
+        option={
+          <Ionicons
+            name="ellipsis-vertical"
+            size={24}
+            color={colors.BLACK}
+            onPress={handleMoreOption}
+          />
+        }
+      />
+      <Text numberOfLines={3} style={styles.description}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit.
+      </Text>
+
+      <View style={styles.actionContainer}>
+        <Pressable style={styles.menu}>
+          <Ionicons name="heart" size={24} color={colors.BLACK} />
+          <Text style={styles.menuText}>0</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Ionicons name="chatbox" size={24} color={colors.BLACK} />
+          <Text style={styles.menuText}>0</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    padding: 16,
+  },
+  description: {
+    fontSize: 16,
+    color: colors.BLACK,
+    marginBottom: 14,
+  },
+  menu: {
+    width: "50%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 16,
+    gap: 4,
+  },
+  menuText: {
+    fontSize: 14,
+    color: colors.GRAY_700,
+  },
+  actionContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    borderTopColor: colors.GRAY_300,
+    borderTopWidth: StyleSheet.hairlineWidth,
+  },
+});
