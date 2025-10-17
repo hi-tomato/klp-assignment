f8017a7fe1ce231aec08369e7d49ca04a4c79c25
이승준 <tmdwns092727@nate.com>
2025-10-17 19:49:03 +0900

    feat: Api Types 추가

---
+++ types/index.ts
@@ -0,0 +1,37 @@
+import { Timestamp } from "firebase/firestore";
+
+interface User {
+  id: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  createdAt: Timestamp;
+}
+
+interface Post {
+  id: string;
+  authorId: string;
+  content: string;
+  imageUrl: string[];
+  createdAt: Timestamp;
+  likeCount: number;
+  commentCount: number;
+}
+
+interface PostLike {
+  userId: string;
+  createdAt: Timestamp;
+}
+
+interface PostComment {
+  id: string;
+  postId: string;
+  authorId: string;
+  content: string;
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+  isDeleted: boolean;
+  authorImageUrl?: string;
+}
+
+export type { Post, PostComment, PostLike };
