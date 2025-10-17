4768b3f60ba2a02b3e898e969cd672cd0ab2b3db
이승준 <tmdwns092727@nate.com>
2025-10-17 14:13:10 +0900

    feat: Post Screen 추가

--- app/post/_layout.tsx
+++ app/post/_layout.tsx
@@ -1,6 +1,8 @@
 import { colors } from "@/constants/colors";
-import { Stack } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { router, Stack } from "expo-router";
 import React from "react";
+import { Pressable } from "react-native";
 
 export default function PostLayout() {
   return (
@@ -8,13 +10,23 @@ export default function PostLayout() {
       screenOptions={{
         headerTintColor: colors.BLACK,
         contentStyle: { backgroundColor: colors.WHITE },
+        headerShown: false,
       }}
     >
       <Stack.Screen
-        name="index"
+        name="[id]"
         options={{
           headerShown: true,
-          title: "",
+          headerTitle: "상세보기",
+          headerLeft: () => (
+            <Pressable
+              onPress={() =>
+                router.canGoBack() ? router.back() : router.replace("/")
+              }
+            >
+              <Feather name="arrow-left" size={28} color={"black"} />
+            </Pressable>
+          ),
         }}
       />
     </Stack>
