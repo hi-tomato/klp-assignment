0c982c4ad6072ed3104a1f3c42b62e7391887e02
이승준 <tmdwns092727@nate.com>
2025-10-17 20:44:09 +0900

    feat: Post Write Screen 관련 컴포넌트 추가
    - TitleInput: 제목 입력 컴포넌트
    - ContentInput: 내용 입력 컴포넌트

--- app/post/write.tsx
+++ app/post/write.tsx
@@ -1,10 +1,74 @@
-import React from "react";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/common/CustomButton";
+import ImagePreview from "@/components/feed/ImagePreview";
+import ContentInput from "@/components/input/ContentInput";
+import TitleInput from "@/components/input/TitleInput";
+import { useCreatePost } from "@/hooks/usePost";
+import { useNavigation } from "expo-router";
+import React, { useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Alert, StyleSheet, View } from "react-native";
+
+type WriteFormValues = {
+  title: string;
+  content: string;
+  imageUrl: string[];
+};
 
 export default function WriteScreen() {
+  const navigation = useNavigation();
+  const { createPost } = useCreatePost();
+  const writeForm = useForm<WriteFormValues>({
+    defaultValues: {
+      title: "",
+      content: "",
+      imageUrl: [],
+    },
+  });
+
+  const onSubmit = async (data: WriteFormValues) => {
+    const { title, content, imageUrl } = data;
+
+    try {
+      await createPost({
+        title,
+        content,
+        imageUrl,
+      });
+      // TODO: Toast로 변경
+      Alert.alert("성공", "게시글이 작성되었습니다!", [{ text: "확인" }]);
+    } catch (error) {
+      Alert.alert("실패", "게시글 작성에 실패했습니다.");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <CustomButton
+          label="저장"
+          size="medium"
+          variant="standard"
+          onPress={writeForm.handleSubmit(onSubmit)}
+        />
+      ),
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Write</Text>
-    </View>
+    <FormProvider {...writeForm}>
+      <View style={styles.formContainer}>
+        <TitleInput />
+        <ContentInput />
+        <ImagePreview imageUris={[]} />
+      </View>
+    </FormProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  formContainer: {
+    margin: 12,
+    gap: 12,
+  },
+});
