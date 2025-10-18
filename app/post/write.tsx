6353b72352ba738ea4c255d4c71033cb3d23bd80
이승준 <tmdwns092727@nate.com>
2025-10-19 00:03:37 +0900

    fix: write deps Fix

--- app/post/write.tsx
+++ app/post/write.tsx
@@ -53,7 +53,7 @@ export default function WriteScreen() {
         />
       ),
     });
-  }, []);
+  }, [navigation, writeForm.handleSubmit, onSubmit]);
 
   return (
     <FormProvider {...writeForm}>
