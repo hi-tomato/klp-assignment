ae1fcfd4b6f615a219266e8a0b69b0bedc8cc317
이승준 <tmdwns092727@nate.com>
2025-10-18 23:56:23 +0900

    fix: _layout.tsx Provider 연동

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,36 @@
+import { useAuthStore } from "@/store/useAuthStore";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import "react-native-reanimated";
+import Toast from "react-native-toast-message";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  return (
+    <ActionSheetProvider>
+      <>
+        <Toast />
+        <RootLayoutNavigator />
+      </>
+    </ActionSheetProvider>
+  );
+}
+
+function RootLayoutNavigator() {
+  const { user } = useAuthStore();
+
+  useEffect(() => {
+    user?.uid &&
+      Toast.show({
+        type: "success",
+        text1: `${user?.displayName ?? "사용자"}님 환영합니다!`,
+      });
+  }, [user]);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
